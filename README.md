7a0745eed3787447fed12d15688ac9453503db25
Baribor Saturday <saturdaybaribor@gmail.com>
2022-11-04 01:59:26 +0100

    Make changes to script to work with the final csv format.

--- README.md
+++ README.md
@@ -1,22 +1,15 @@
 ## HOW TO USE THE SCRIPT
 Firstly you have to clone the repo
 
-`git clone https://github.com/Baribor/hng9-chip-007.git`
-
-After that, to run the script, you run the command below:
-
+`git clone https://github.com/Baribor/hng9-chip-007.git`\
+Navigate to the new folder:\
+`cd hng9-chip-007`\
+After that, to run the script, you run the command below:\
+Install the required library:\
+`pip install -r requirements.txt`\
+Run the script:\
 `python worker.py`
 
-This assumes you have python installed and properly setup on your device.
+These assume you have Python installed and properly setup on your device.
 
-## PROJECT STRUCTURE
-The [input files](input%20files) folder serves as a folder to put all csv files to be processed. Currently only a file - [Team Bevel.csv](input%20files/Team%20Bevel.csv) is there.
-For the script to work according, all csv files must adhere strictly to the following headings:
-
-**Series Number**\
-**Filename**\
-**Description**\
-**Gender**\
-**UUID**\
-**Hash**\
-These were picked based on the priority of the team's presentation.
+**Note:** You internet connection should be active to run the script as the data needs to be pulled from the google sheet online.
--- worker.py
+++ worker.py
@@ -1,140 +1,121 @@
-#pylint:disable=W0603
-# pylint:disable=W0621
 import csv
 import json
-import hashlib
 import os
+import hashlib
+import requests
 
+SHEET_ID ='1b5H3bp_9-YVjTYQNjLeokXJewrcPfgUo_MYvYXtaUno'
 
-team_data = {
-    "Team Bevel": (1, 20),
-    "Team Clutch": (21, 40),
-    "Team Engine": (41, 60),
-    "Team Grit": (61, 80),
-    "Team Prybar": (81, 100),
-    "Team Chisel": (101, 120),
-    "Team Brainbox": (121, 140),
-    "Team Plug": (141, 160),
-    "Team Headlight": (161, 180),
-    "Team Crankshaft": (181, 200),
-    "Team Gear": (201, 220),
-    "Team Tape": (221, 240),
-    "Team Axle": (241, 260),
-    "Team PowerDrill": (261, 280),
-    "Team Hydraulics": (281, 300),
-    "Team Scale": (301, 320),
-    "Team Sandpaper": (321, 340),
-    "Team Ruler": (341, 360),
-    "Team Vbelt": (361, 380),
-    "Team Axe": (381, 400),
-    "Team Boot ": (401, 420),
-}
+SHEET_NAME = 'Sheet1'
 
-data = []
+url = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
+
 hashes = {}
-sn, file_name, desc, gender, uuid, _ = [None] * 6
-column2index = {}
 
+def download_csv():
+    print("Downloading data...")
+    response = requests.get(url)
+    
+    with open('data.csv', 'w') as file:
+        file.write(response.content.decode('utf-8'))
+    print('Done')
+    
 
-def get_team_name(index):
-    """
-    Given an a serial number, returns the name of the team that is assigned the serial number.
-    """
-    for key, value in team_data.items():
-        if index in range(value[0], value[1] + 1):
-            return key
+def generate_json():
+    
+    if not os.path.exists('jsons'):
+        os.mkdir('jsons')
 
-
-def create_json():
-    global column2index, file_name
-
-    if not os.path.exists("output files"):
-        os.mkdir("output files")
-
-    for file in os.listdir("input files"):
-
-        with open(f"input files/{file}") as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=",")
-            line_count = 0
-
-            for row in csv_reader:
-                if line_count == 0:
-                    sn, file_name, desc, gender, uuid, _ = row
-                    column2index = {n: i for i, n in enumerate(row)}
-                    print(f'Column names are {", ".join(row)}')
-                    line_count += 1
-                else:
-
-                    item = {
-                        "format": "CHIP-0007",
-                        "name": row[column2index[file_name]],
-                        "description": row[column2index[desc]],
-                        "minting_tool": get_team_name(int(row[column2index[sn]])),
-                        "sensitive_content": False,
-                        "series_number": row[column2index[sn]],
-                        "series_total": 526,
-                        "attributes": [
-                            {
-                                "trait_type": "gender",
-                                "value": row[column2index[gender]],
-                            },
-                            {"trait_type": "uuid", "value": row[column2index[uuid]]},
-                        ],
-                        "collection": {
-                            "name": "Zuri NFT Tickets for Free Lunch",
-                            "id": "b774f676-c1d5-422e-beed-00ef5510c64d",
+    with open("data.csv") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
+        current_team = ''
+        
+    
+        for line,row in enumerate(csv_reader):
+            team, sn, filename, name, description, gender, attributes, _ = row[:8]
+            
+            if line== 0:
+              
+                print(f'HEADERS:  {", ".join([x for i,x in enumerate(row) if i<8])}')
+                print()
+                print('Starting CHIP-007 json creation...')
+                
+            else:
+                if team:
+                    current_team = team
+                    
+                item = {
+                            "format": "CHIP-0007",
+                            "name": name,
+                            "description": description,
+                            "minting_tool": current_team,
+                            "sensitive_content": False,
+                            "series_number": sn,
+                            "series_total": 526,
                             "attributes": [
                                 {
-                                    "type": "description",
-                                    "value": "Rewards for accomplishments during HNGi9.",
-                                }
+                                    "trait_type": "gender",
+                                    "value": gender,
+                                },
+                                
                             ],
-                        },
-                    }
-                    file = f"{row[column2index[file_name]]}.json"
-                    with open(f"output files/{file}", "w") as output:
-                        output.write(json.dumps(item, indent=4))
-                    data.append(file)
+                            "collection": {
+                                "name": "Zuri NFT Tickets for Free Lunch",
+                                "id": "b774f676-c1d5-422e-beed-00ef5510c64d",
+                                "attributes": [
+                                    {
+                                        "type": "description",
+                                        "value": "Rewards for accomplishments during HNGi9.",
+                                    }
+                                ],
+                            },
+                        }
+                attr = [x.split(':') for x in attributes.split(';') if x]
+                
+                for att in attr:
+                    item['attributes'].append({'trait_type': att[0].strip(), 'value':att[1].strip()})
+                
+                with open(f'jsons/{filename}.json', 'w') as jf:
+                    jf.write(json.dumps(item, indent=4))
+                    
+    print('Done')
+                    
+                    
+        
+                
+                
+def generate_hash():
+    print('Starting sha256 hashing...')
+    for f in os.listdir('jsons'):
+         with open(f'jsons/{f}', 'rb') as jf:
+                hashes['.'.join(f.split('.')[:-1])] = hashlib.sha256(jf.read()).hexdigest()
+    print('Done')
 
+                        
 
-def generate_hashes():
-    """
-    Generates the sha256 hash for each created json file.
-    """
-    for f in data:
-        with open(f"output files/{f}", "rb") as file:
-            h = hashlib.sha256(file.read()).hexdigest()
-            hashes[f.split(".")[0]] = h
-
-
-def create_output():
-    """
-    Creates an output file with the name format file_name.output.csv with the hash of each row appended.
-    """
-
-    if not os.path.exists("outputs"):
-        os.mkdir("outputs")
-
-    for file in os.listdir("input files"):
-        with open(f"input files/{file}", "r") as read_file, open(
-            f'outputs/{file.split(".")[0]}.output.csv', "w"
-        ) as write_file:
-            cr = csv.reader(read_file)
-            cw = csv.writer(write_file)
-            line_count = 0
-            for row in cr:
-                if line_count == 0:
-                    row.append("CHIP Hash-sha256")
-                    line_count += 1
-
+def generate_output():
+    print('Generating output...')
+    if not os.path.exists('output'):
+        os.mkdir('output')
+        
+    
+    with open('data.csv', 'r') as read_file, open('output/data.output.csv', 'w') as write_file:
+          cr = csv.reader(read_file)
+          cw = csv.writer(write_file)
+          
+          for line, data in enumerate(cr):
+                row = data[:8]
+                if line==0:
+                    row.append('Hash')
                 else:
-
-                    row.append(hashes[row[column2index[file_name]]])
+                    row.append(hashes[row[2]])
                 cw.writerow(row)
+    print('Done')
+                
 
 
-if __name__ == "__main__":
-
-    create_json()
-    generate_hashes()
-    create_output()
+if __name__=='__main__':
+    download_csv()
+    generate_json()
+    generate_hash()
+    generate_output()
